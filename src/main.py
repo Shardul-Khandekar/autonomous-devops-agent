0197cb0218998a29a8a2f30620c141be004a9b1e
Shardul Khandekar <khandekarshardul@gmail.com>
2025-10-25 23:15:05 -0400

    providing monitoring solution using langfuse

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 import argparse
 from crewai import Crew, Process
+from langfuse import get_client
+import openlit
 
 from agents.architect import architect_agent
 from tasks.vision_tasks import analysis_task
 
 from agents.surveyor import surveyor_agent
 from tasks.query_tasks import query_task
+
+# Initialize Langfuse callback handler
+langfuse = get_client()
+
+if langfuse.auth_check():
+    print("Langfuse client is authenticated and ready!")
+else:
+    print("Authentication failed. Please check your credentials and host.")
+
+# Initialize openlit
+openlit.init()
 
 
 def run_crew(image_path: str):
